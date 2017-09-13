8fcca58de50f9fce6426359fed4327dba4d07cd3
Karime Saad <karime.saad@utexas.edu>
2017-09-12 21:40:33 -0500

    added Lab 2 project - prep

---
+++ Labs/Lab_2/Lab_2-ks38728_tk8435_PMF/main.d
@@ -0,0 +1,7 @@
+.\main.o: main.c
+.\main.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\main.o: ADCSWTrigger.h
+.\main.o: ../inc/tm4c123gh6pm.h
+.\main.o: PLL.h
+.\main.o: Timer1.h
+.\main.o: ST7735.h
