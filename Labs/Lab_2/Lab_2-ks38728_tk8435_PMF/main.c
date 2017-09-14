6bd569e83f38414f3b2b8ea10e114dff2a1c6f3a
Karime Saad <karime.saad@utexas.edu>
2017-09-13 23:50:26 -0500

    updated lab 2

--- Labs/Lab_2/Lab_2-ks38728_tk8435_PMF/main.c
+++ Labs/Lab_2/Lab_2-ks38728_tk8435_PMF/main.c
@@ -31,9 +31,12 @@
 #include "PLL.h"
 #include "Timer1.h"
 #include "ST7735.h"
+#include "stdio.h"
 
 #define PF2             (*((volatile uint32_t *)0x40025010))
 #define PF1             (*((volatile uint32_t *)0x40025008))
+#define PF4   (*((volatile uint32_t *)0x40025040))
+
 #define PMF_MAX_SIZE 4096
 #define ARR_SIZE 1000
 void DisableInterrupts(void); // Disable interrupts
@@ -48,11 +51,13 @@ void CalculateYAxis(void);
 void DrawPMF(void);
 void ResetScreen(void);
 void PortF_Init(void);
+void Pause(void);
+void DelayWait10ms(uint32_t n);
+void ST7735_OutNum(char *ptr);
 
 
 
-//***********TODO: do we need to use volatile?***********
-
+/****** Global Variables *******/
 volatile uint32_t ADCvalue;
 uint32_t timeStamps[ARR_SIZE] = {0};
 uint32_t adcValues[ARR_SIZE] = {0};
@@ -63,7 +68,6 @@ uint32_t pmfMinX = 0;
 uint32_t pmfMaxX = 0;
 uint32_t pmfMinY = 0;
 uint32_t pmfMaxY = 0;
-uint32_t calculating = 1;
 
 // This debug function initializes Timer0A to request interrupts
 // at a 100 Hz frequency.  It is similar to FreqMeasure.c.
@@ -95,11 +99,11 @@ void Timer0A_Handler(void){
   ADCvalue = ADC0_InSeq3();
 	PF2 ^= 0x04;                   // profile
 
-	if(currIndex < ARR_SIZE){
-		timeStamps[currIndex] = TIMER1_TAR_R;
-		adcValues[currIndex] = ADCvalue;
-		currIndex ++;
-	}
+//	if(currIndex < ARR_SIZE){
+//		timeStamps[currIndex] = TIMER1_TAR_R;
+//		adcValues[currIndex] = ADCvalue;
+//		currIndex ++;
+//	}
 }
 int main(void){
   PLL_Init(Bus80MHz);                   // 80 MHz
@@ -108,22 +112,25 @@ int main(void){
   ADC0_InitSWTriggerSeq3_Ch9();         // allow time to finish activating
   Timer0A_Init100HzInt();               // set up Timer0A for 100 Hz interrupts
 	PortF_Init();	
-	Timer1_Init();
-	ResetScreen();
-
+//	Timer1_Init();
 
   EnableInterrupts();
 	
 	while(1){
 		
-		while(currIndex < ARR_SIZE){
+//		while(currIndex < ARR_SIZE){
+//			PF1 ^= 0x02;  // toggles when running in main
+//		}
 			PF1 ^= 0x02;  // toggles when running in main
-		}
-		DisableInterrupts();
+
 		
-		CalculateJitter();
-		CalculatePMF();
-		DrawPMF();	
+//	DisableInterrupts();
+		
+//		CalculateJitter();
+//		Pause();
+//		CalculatePMF();
+//		DrawPMF();
+		
 	}
 }
 
@@ -141,7 +148,23 @@ void CalculateJitter(void){
 			largestTimeDiff = delta;
 		}	
 	}
-	jitter = smallestTimeDiff - largestTimeDiff;
+	jitter = largestTimeDiff - smallestTimeDiff;
+	ResetScreen();
+	char* jitterVal = "jitter: ";
+	ST7735_SetCursor(1,1);
+	ST7735_OutString(jitterVal);
+	
+	char arr[4] = {0};
+
+		jitter = jitter / 10;
+		arr[2] = (jitter % 10) + '0';
+		jitter = jitter / 10;
+		arr[1] = (jitter % 10) + '0';
+		jitter = jitter / 10;
+		arr[0] = (jitter % 10) + '0';
+
+	
+	ST7735_OutString(arr);
 }
 
 void CalculatePMF(void){
@@ -180,12 +203,14 @@ void CalculateYAxis(void){
 }
 
 void DrawPMF(void){
+		ResetScreen();
 	for(uint32_t x = pmfMinX; x<pmfMaxX; x++){
 		if(adcValues[x] != 0){
-			ST7735_DrawFastVLine(adcValues[x], 20, pmf[adcValues[x]], ST7735_WHITE);
+			ST7735_PlotBar(pmf[adcValues[x]]);
+//			ST7735_DrawFastVLine(adcValues[x], 20, pmf[adcValues[x]], ST7735_WHITE);
 		}
+		ST7735_PlotNext();
 	}
-	calculating = 0;
 }
 
 void ResetScreen(void){
@@ -203,3 +228,36 @@ void PortF_Init(void){
   GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
   PF2 = 0;                      // turn off LED
 }
+
+/*******Name: Pause**********
+// Description: Used to check if PF4, which is connected to switch 1, 
+                has been pressed or not. 
+// Inputs: None
+// Outputs: None
+// Notes: This function was provided to us.
+************************************/
+void Pause(void){
+  while(PF4==0x00){ 
+    DelayWait10ms(10);
+  }
+  while(PF4==0x10){
+    DelayWait10ms(10);
+  }
+}
+/*******Name: DelayWait10ms**********
+// Description: Subroutine to wait 10 msec
+// Inputs: None
+// Outputs: None
+// Notes: This function was provided to us
+************************************/
+void DelayWait10ms(uint32_t n){
+	uint32_t volatile time;
+  while(n){
+    time = 727240*2/91;  // 10msec
+    while(time){
+	  	time--;
+    }
+    n--;
+  }
+}
+
